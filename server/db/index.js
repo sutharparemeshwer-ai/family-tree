0b003f4e065d7c6a597260749d411a0c5b77c4bb
sutharparemeshwer-ai <sutharparemeshwer@gmail.com>
2025-12-16 09:41:46 +0530

    prepair for devlopment

--- server/db/index.js
+++ server/db/index.js
@@ -1,12 +1,21 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-  user: 'parm',
-  host: 'localhost',
-  database: 'family_tree_db',
-  password: 'a1rtp2er',
-  port: 5432,
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {
+        user: 'parm',
+        host: 'localhost',
+        database: 'family_tree_db',
+        password: 'a1rtp2er',
+        port: 5432,
+      }
+);
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
